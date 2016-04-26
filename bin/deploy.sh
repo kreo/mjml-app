b6eb6f1e3f37c2f5a316eb178f9a741b21165576
Guillaume Badi <gbadi@student.42.fr>
2016-04-26 15:12:38 +0200

    deploy removed unecessary pull

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,7 +3,6 @@
 git checkout gh-pages &&
   find . -maxdepth 1 -not -name node_modules -not -name '.*' | xargs rm -rf &&
   cp -r .dist/* . &&
-  git pull origin gh-pages &&
   git add . &&
   git commit -m 'deploy' &&
   git push origin gh-pages
