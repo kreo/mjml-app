7afcd9599367f603bdc3485b09b0f56e15713ee1
Guillaume Badi <gbadi@student.42.fr>
2016-04-26 14:51:28 +0200

    undo

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,8 +3,9 @@
 git checkout gh-pages &&
   find . -maxdepth 1 -not -name node_modules -not -name '.*' | xargs rm -rf &&
   cp -r .dist/* . &&
+  git pull origin gh-pages &&
   git add . &&
   git commit -m 'deploy' &&
-  git push origin master
+  git push origin gh-pages
   git checkout site &&
   printf '\n>everything has been done, master.'
