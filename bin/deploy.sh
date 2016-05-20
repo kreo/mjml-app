28396f0b627eb27c94fd2d8cfdefc5e740f7c089
meriadec <meriadec.pillet@gmail.com>
2016-05-20 21:22:55 +0200

    update deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,6 +3,7 @@
 git checkout gh-pages &&
   find . -maxdepth 1 -not -name node_modules -not -name 'favicon.ico' -not -name '.*' | xargs rm -rf &&
   cp -r .dist/* . &&
+  git add --all &&
   git add . &&
   git commit -m 'deploy' &&
   git push -f origin gh-pages
